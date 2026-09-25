624fa28493a2a7a837341f6e6a47671689d5927a
Harshitha <harshitha13022007@gmail.com>
2026-09-25 23:49:45 +0530

    draw the circle

--- sketch.js
+++ sketch.js
@@ -1,14 +1,31 @@
 const r = require("raylib");
 
+const WIDTH = 1000;
+const HEIGHT = 1000;
+
+const FPS = 50;
+
 function running() {
     return !r.WindowShouldClose();
 }
 
-function setup() { }
+function setup() {
+    r.InitWindow(WIDTH, HEIGHT, "Moving Circle");
+    r.SetTargetFPS(FPS);
+}
 
 function update() { }
 
-function draw() { }
+function draw() {
+    r.BeginDrawing();
+    r.ClearBackground(r.WHITE);
+    const radius = 100;
+    const y = 300;
+    const x = 200;
+    r.DrawCircle(x, y, radius, r.RED);
+    r.EndDrawing();
+
+}
 
 function teardown() {
     return r.CloseWindow();
